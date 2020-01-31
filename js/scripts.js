fd9fdcaa63917e5714bc1e8f671c1a89eb6cfc28
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-31 09:59:31 -0800

    scripts written out, taking value but not printing. Go through code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,70 @@
 // back end 
-function Pizza(){
+function Pizza(size, toppings, cost, totalCost){
   this.size = size;
   this.toppings = toppings;
   this.cost = cost
-  this.totalCost = totalCost;
+  this.totalCost = [];
 }
-
-
 
 Pizza.prototype.totalCost = function displayAmount(){
   var sizeRequested = parseInt($("radio#value").val());
   var toppingsRequested = parseInt($("checkbox#value").val());
-  var totalCostprint = addSizeToCost(sizeRequested += toppingsRequested);
-  return totalCostprint;
+  var totalCostprint = addToppingsToCost(sizeRequested += toppingsRequested)
+  this.totalCost.push(totalCostprint);
 }
 
 
-function addSizeToCost(size, toppings, cost){
+function addSizeToCost(size, cost){
   var cost = 0;
   this.size = function(){
     if(size === 8){
       size += cost;
-      console.log(cost);
+      this.totalCost.push(cost);
     } else if(size === 10){
       size += cost;
+      this.totalCost.push(cost);
     } else if(size === 12){
       size += cost;
+      this.totalCost.push(cost);
     } else{
       alert("Please choose a size")
-    }
-  };
-
-  this.toppings = function(){
-    this.toppings = function(){
-      if(toppings === 1){
-        toppings += size;
-      } else{
-      }
     }
   }
 }
 
-
+function addToppingsToCost(toppings, cost){
+  var cost = addSizeToCost(cost);
+  this.toppings = function(){
+    if(toppings === 1){
+      toppings += cost;
+      this.totalCost.push(cost);
+    } else {
+      this.totalCost.push(cost);
+    }
+  }
+}
 
 // user interface 
 
+var pizza = new Pizza();
 
+function newOrder (totalCost){
+  var customer = new Pizza (totalCost);
+  Pizza.totalCost(customer);
+  return customer;
+}
 
-
-
-
+function displayTotal (){
+  $("span#orderTotal").text("$" + this.totalCost);
+}
 
 // front end 
 
 $(document).ready(function(){
   $("button#completeOrder").submit(function(event){
     event.preventDefault();
-    var total = Pizza.totalCost;
-    $("#orderTotal").html(total);
+    var total = newOrder()
+    displayTotal(total);
 
   })
 })
