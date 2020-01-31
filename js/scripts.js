f4ae75164f6d311f7839c8442efef7eb6bafcae6
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-31 14:08:34 -0800

    refactor some code, change values in HTML, try to push one output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,47 @@
 // back end 
 function Pizza(size, toppings){
   this.size = size,
-  this.toppings = toppings
+  this.toppings = toppings;
 }
 
-Pizza.prototype.addTopping = function(){
-  this.toppings.push();
-};
 
 Pizza.prototype.addToCost = function(){
-  var cost;
-    if(this.size === 8){
-      cost = 8;
-    } else if(this.size === 10){
-      cost = 10;
-    } else if(this.size === 12){
-      cost = 12;
+  var cost = 0;
+    if(this.size === "small"){
+      cost += 8;
+    } else if(this.size === "medium"){
+      cost += 10;
+    } else if(this.size === "large"){
+      cost += 12;
     } ;
-    // ??for loop going through each checkbox value for toppings??
-    for(var i = 0; i < this.toppings; i++){
-      var toppingAdd = ["pepperoni", "extra cheese", "anchovies", "pineapple"]
-      if (toppingAdd.includes(this.toppings[i])){
+    if(this.toppings === checked){
       cost += 1;
-      }; 
-  };
-  return cost;
+    }
+    return cost;
 };
+
+Pizza.prototype.addTop = function(topping){
+  this.toppings.push(topping);
+}
 
 
 // front end 
 
 $(document).ready(function(){
-  $("#completeOrder").submit(function(event){
+  $("form#pizzaOrder").submit(function(event){
     event.preventDefault();
+
     var size = $("input:radio[name=size]:checked").val();
     var toppings = [];
-    $("input:checkbox[name=toppings]:checked").each(function() {
-    var checkTopping = $(this).val();
-      toppings.push(checkTopping);
-      toppings.push(size);
-    }); 
+
+    var finalPizza = new Pizza(inputSize, inputToppings);
+
+    $("input:checkbox[name=toppings];checked").each(function(){
+      var toppingsPicked = $(this).val();
+      finalPizza.addTop(toppingsPicked)
+    })
   });
+  var finalPrice = finalPizza.addToCost();
+
+  $("span.orderTotal").html(finalPrice)
 });
